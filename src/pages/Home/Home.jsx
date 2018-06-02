bc0856d6537559ede8721defdf72a6dbad47362f
kun368 <kun368@126.com>
2018-06-02 15:22:11 +0800

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import ExcellentHomePage from "./components/ExcellentHomePage/ExcellentHomePage";
 import ProductInfo from "./components/ProductInfo/ProductInfo";
+import {Dialog} from "@icedesign/base/index";
 
 export default class Home extends Component {
   static displayName = 'Home';
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      welcomeDialogShow: true,
+    };
+  }
+
+  onWelcomeDialogClose = () => {
+    this.setState({
+      welcomeDialogShow: false
+    });
+  };
+
+  renderWelcomeDialog() {
+    return (
+      <Dialog
+        visible={this.state.welcomeDialogShow}
+        onOk={this.onWelcomeDialogClose}
+        closable="esc,mask,close"
+        onCancel={this.onWelcomeDialogClose}
+        onClose={this.onWelcomeDialogClose}
+        title="欢迎使用星云考勤系统"
+      >
+        <p style={{color: 'red'}}>本应用已完美支持手机扫码+手机钱包考勤！</p>
+        <p>
+          如何避免考勤作弊？<br/>
+          如何更加灵活地安排考勤流程？<br/>
+          如何更合理有效地分析运用考勤数据？…… <br/>
+          我们通过创造性地运用区块链技术，为考勤工具带来了全新的可能。<br/>
+          星云考勤系统，基于星云智能合约，数据不可篡改，无法作弊，简化考勤流程<br/>
+        </p>
+        <p style={{color: 'red'}}>您可以点击“使用帮助”查看详细的考勤使用过程。</p>
+      </Dialog>
+    );
   }
 
   render() {
     return (
       <div className="home-page">
+        {this.renderWelcomeDialog()}
         <ExcellentHomePage/>
         <ProductInfo/>
       </div>
